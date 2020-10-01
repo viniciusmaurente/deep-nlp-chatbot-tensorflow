ba27d01437640e4e79430af966315feb3daaf8b0
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-09-30 23:54:29 -0300

    criate a dictionary to map each line with its ID

--- chatbot_tensorflow.py
+++ chatbot_tensorflow.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 30 22:00:20 2020
+Created on 2020
 
 @author: viniciusmaurente
 """
+
+#create chatbot whith Deep NLP
+
+#import library
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+# -- first step 1 - pré processing data
+
+#import database
+linhas = open('movie_lines.txt', encoding ='utf-8', errors = 'ignore').read().split('\n')
+conversas = open('movie_conversations.txt', encoding ='utf-8', errors = 'ignore').read().split('\n')
+
+#criação de um dicionário para mapear cada linha com seu ID
+id_para_linha = {}
+for linha in linhas:
+    #print (linha)
+    _linha = linha.split(' +++$+++ ')
+    #print (_linha)
+    if len(_linha) == 5:
+    #print(_linha[4])
+        id_para_linha[_linha[0]] = _linha[4]
+        
+#criação de uma lista com todas as conversas
